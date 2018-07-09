3490660bfe50a83c93b03fdd04e54ea84437bef5
Rena Tan <rena.tanrx@gmail.com>
2018-07-08 17:17:25 -0700

    more exercies

--- testexercises.js
+++ testexercises.js
@@ -27,9 +27,6 @@ for(var i = 0; i <students.length; i++){
 	myH3.innerText = myH3.innerText + students[i].isEnrolled + " , ";
 }
 
-
-
-
 // Different type of exercise: Higher order functions using .filter() method:
 // creates a new array and expects true or false value
 
@@ -46,17 +43,28 @@ var animals = [
 	{name: 'Doc', species: 'bird'}
 ];
 
-
+// first optional answer:
 var animalsType = function(item){
 
 	return animals.filter(function(el){
-		if(item === el.species || item === el.name)
-			return el;
-		})
+		return item === el.species;
+		});
 };
 
-animalsType('Maud');
-animalsType('cat');
+animalsType('dog');
+
+// second optional answer:
+const arraySearchBySpecies = (array, type) => array.filter(item => item.species === type);
+const arraySearchByName = (array, type) => array.filter(item => item.name === type);
+
+arraySearchByName(animals, 'Maud');
+arraySearchBySpecies(animals, 'bird');
+
+
+// ES06 version:
+
+const prettyPrint = (item) => `${item.name}: ${item.species}`;
+console.log(prettyPrint(animals[0]));
 
 
 
@@ -69,7 +77,7 @@ var cars = [
 	{model: 'Toyota', year: 1992, color: 'red'},
 	{model: 'Subaru', year: 2010, color: 'black'},
 	{model: 'Hyndai', year: 1993, color: 'white'},
-	{model: 'Ford', year: 2015, color: 'red'}
+	{model: 'Ford', year: 2015, color: 'blue'}
 ];
 
 // ES5:
@@ -78,10 +86,23 @@ var carType = cars.map(function(item){
 	' and last, but not least, the color is ' + item.color + '.';
 });
 
+// ES06:
+const vehiclesOfColor = (color) => cars.filter(car => car.color === color);
+const blueCars = vehiclesOfColor('blue');
+blueCars.forEach(console.log); //now it shows all blue cars.
+
+vehiclesOfColor('blue');
+
+const vehiclesFromYear = (year) => cars.filter(car => car.year === year);
+vehiclesFromYear(2001);
+
+const vehiclesByModel = (model) => cars.filter(car => car.model === model); 
+vehiclesByModel('Ford');
+
+
 // ES6:
 
 var carType = cars.map((item) => `Here is the model: ${item.model}. This is the color: ${item.color}. And, last but not least, this is the year: ${item.year}.`);
-
 console.log(carType);
 
 
@@ -106,22 +127,25 @@ var totalAmt = orders.reduce(function(sum, order){
 console.log(totalAmt);
 
 
-//Advance form of .reduce(): It's not just for lists; can .reduce() an object or another array.
 
+const customersGroup =
 {
 	'Mark Johnson': [
-		{name: 'waffle iron', price: '80', quantity: '2'},
-		{name: 'ties', price: '200', quantity: '3'},
-		{name: 'knife', price: '15', quantity: '5'}
+		{name: 'waffle iron', price: 80, quantity: 2},
+		{name: 'ties', price: 200, quantity: 3},
+		{name: 'knife', price: 15, quantity: 5}
 	],
 	'Sally Howard': [
-		{name: 'blow dryer', price: '180', quantity: '1'},
-		{name: 'pot', price: '55', quantity: '3'},
-		{name: 'frying pan', price: '92', quantity: '1'},
-	];
-}
+		{name: 'blow dryer', price: 180, quantity: 1},
+		{name: 'pot', price: 55, quantity: 3},
+		{name: 'frying pan', price: 92, quantity: 1}
+	]
+};
+
+const customerBill = (customer) => customersGroup[customer].reduce((runningTotal, item) => 
+runningTotal + item.price * item.quantity, 0);
 
 
+const prettyPrint = (customerItem) => `Here is the total amount owed: $${customerItem}.00`;
 
-
-
+prettyPrint(customerBill('Mark Johnson'));
